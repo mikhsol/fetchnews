88306c1c6ca391f42235f26a84072bd6fcb47d3e
Mikhail <miksolodovnikov@gmail.com>
2017-08-18 16:57:42 +0500

    Add id to article based on hask of url and date.

--- src/entities.py
+++ src/entities.py
@@ -1,3 +1,6 @@
+import hashlib
+
+
 class Author(object):
     def __init__(self, first_name=None, second_name=None,
                  position=None, twitter=None):
@@ -9,8 +12,10 @@ class Author(object):
 
 class Article(object):
     def __init__(self, author=None, headline=None, text=None,
-                 url=None):
+                 url=None, date=None):
         self.author = author
         self.headline = headline
         self.text = text
         self.url = url
+        self.date = date
+        self.id = hashlib.sha3_256(bytes(url + date, 'utf-16be')).hexdigest()
--- test/test_entities.py
+++ test/test_entities.py
@@ -1,3 +1,4 @@
+import hashlib
 import unittest
 
 from src.entities import Author, Article
@@ -28,15 +29,19 @@ TEXT = "Labor has opened a new front in the Turnbull government’s" \
 URL = "https://www.theguardian.com/australia-news/2017/aug/18/" \
       "labor-questions-if-joyce-and-nash-can-make-legally-valid" \
       "-decisions-as-ministers"
+DATE = "Friday 18 August 2017 12.36 BST"
+ID = hashlib.sha3_256(bytes(URL+DATE, 'utf-16be')).hexdigest()
 
 
 class TestArticle(unittest.TestCase):
     def test_new_article(self):
-        article = Article(AUTHOR, HEADLINE, TEXT, URL)
+        article = Article(AUTHOR, HEADLINE, TEXT, URL, DATE)
+        self.assertEqual(article.id, ID)
         self.assertEqual(article.author, AUTHOR)
         self.assertEqual(article.headline, HEADLINE)
         self.assertEqual(article.text, TEXT)
         self.assertEqual(article.url, URL)
+        self.assertEqual(article.date, DATE)
 
 
 if __name__ == '__main__':
