af3e5ab14a98b69dfaee11f116662d2b10c7fa66
Mikhail <miksolodovnikov@gmail.com>
2017-08-22 14:48:37 +0500

    Add get_headline method to html processor. Refactor test and html processor.

--- test/test_html_processor.py
+++ test/test_html_processor.py
@@ -3,27 +3,32 @@ import unittest
 from bs4 import BeautifulSoup
 
 AUTHORS = ["Calla Wahlquist", "Gareth Hutchens"]
+HEADLINE = "Coalition says citizenship crisis will last months but MPs " \
+           "will keep voting"
 
 
 class HtmlProcessor(object):
     def __init__(self, html=None):
-        self.html = html
+        self.soup = BeautifulSoup(html, "html.parser")
 
     def get_authors(self):
-        soup = BeautifulSoup(self.html, "html.parser")
         authors = []
-        for a in soup.find_all("a", {"rel": "author"}):
+        for a in self.soup.find_all("a", {"rel": "author"}):
             authors.append(a.span.text)
 
         return authors
+
+    def get_headline(self):
+        return self.soup.find("h1", {"class": "content__headline"})\
+            .text.strip()
 
 
 class TestArticleProcessor(unittest.TestCase):
     def setUp(self):
         with open("test/data/raw_html.txt", "r") as f:
             raw_html = f.read()
-        p = HtmlProcessor(raw_html)
-        self.authors = p.get_authors()
+        self.p = HtmlProcessor(raw_html)
+        self.authors = self.p.get_authors()
 
     def test_get_author_details(self):
         self.assertTrue(self.authors[0] in AUTHORS)
@@ -31,3 +36,6 @@ class TestArticleProcessor(unittest.TestCase):
     def test_get_few_authors_details(self):
         for a in self.authors:
             self.assertTrue(a in AUTHORS)
+
+    def test_get_headline(self):
+        self.assertEqual(self.p.get_headline(), HEADLINE)
